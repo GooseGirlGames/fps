f5d01be30c88d48de287fb0a915df23b13232931
Jakob Ruckel <jakob@ruckel.de>
2022-12-09 19:54:07 +0100

    Sphere gun goes boom

--- Assets/PhysicsGun.cs
+++ Assets/PhysicsGun.cs
@@ -1,25 +1,47 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PhysicsGun : MonoBehaviour {
 
     [SerializeField]
-    private List<InputActionReference> m_ShootActions = new List<InputActionReference>();
+    private InputActionReference m_ShootAction;
 
     [SerializeField]
-    private Vector3 m_ShootDirection;
+    private Transform m_GunSource;
+
+    [SerializeField]
+    [Range(0.01f, 20f)]
+    private float m_GunStrength;
+
+    private Rigidbody m_Rigidbody;
+
+
+    public Vector3 ShootDirection {
+        get => m_GunSource.forward;
+    }
 
     void Start() {
-        m_ShootDirection.Normalize();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update() {
-        
+        ProcessInputs();
     }
+
 
     void OnDrawGizmos() {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + 5f * m_ShootDirection);
+        Gizmos.DrawLine(m_GunSource.position, m_GunSource.position + 5f * ShootDirection);
+    }
+
+    private void ProcessInputs() {
+        if (m_ShootAction.action.WasPressedThisFrame()) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        Debug.Log("Boom");
+        m_Rigidbody.AddForce((-ShootDirection) * m_GunStrength, ForceMode.Impulse);
     }
 }
