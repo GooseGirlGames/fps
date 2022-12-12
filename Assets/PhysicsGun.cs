b7094dcb12addc80a6a0cab089d8d5d5a8ff59dd
Jakob Ruckel <jakob@ruckel.de>
2022-12-12 15:23:23 +0100

    Mouse control and lasers in progress

--- Assets/PhysicsGun.cs
+++ Assets/PhysicsGun.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,25 +17,48 @@ public class PhysicsGun : MonoBehaviour {
     [SerializeField]
     private Rigidbody m_FishBody;
 
-    private Rigidbody m_Rigidbody;
+    [SerializeField]
+    private LineRenderer m_LineRend;
 
+    private Rigidbody m_Rigidbody;
+    private int m_ArmNumber;
 
     public Vector3 ShootDirection {
         get => m_GunSource.forward;
     }
 
+    public string KeyName {
+        get => m_ShootAction.action.bindings[0].ToDisplayString();
+    }
+
+    private bool m_LineRendEnabled = false;
+
     void Start() {
         m_Rigidbody = GetComponent<Rigidbody>();
+        var actionname = m_ShootAction.action.name;
+        var actionid_str = actionname[actionname.Length - 1];
+        m_ArmNumber = Int32.Parse("" + actionid_str);
     }
 
     void Update() {
         ProcessInputs();
-    }
+        ProcessMouseInput();
 
+        m_LineRend.enabled = m_LineRendEnabled;
+        if (m_LineRendEnabled) {
+            var line_length = 20f;
+            UpdateLineRendererPositions(transform.position, transform.position + (line_length * ShootDirection));
+        }
+    }
 
     void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(m_GunSource.position, m_GunSource.position + 5f * ShootDirection);
+    }
+
+    private void UpdateLineRendererPositions(Vector3 origin, Vector3 destination) {
+        m_LineRend.SetPosition(0, origin);
+        m_LineRend.SetPosition(1, destination);
     }
 
     private void ProcessInputs() {
@@ -43,10 +67,25 @@ public class PhysicsGun : MonoBehaviour {
         }
     }
 
-    private void Shoot() {
+    private void ProcessMouseInput() {
+        var gun_pos = PositionInScreeenSpace();
+        var mouse_pos = Mouse.current.position.ReadValue();
+        var dist = Vector2.Distance(gun_pos, mouse_pos);
+        if (m_ArmNumber == 1) {
+            Debug.Log(dist);
+        }
+    }
+
+    public void Shoot() {
         Debug.Log("Boom");
         var force = (-ShootDirection) * m_GunStrength;
         m_Rigidbody.AddForce(force, ForceMode.Impulse);
         m_FishBody.AddForce(0.5f * force, ForceMode.Impulse);
+    }
+
+    public Vector2 PositionInScreeenSpace() {
+        var camera = Camera.current;
+        var pos = camera.WorldToScreenPoint(this.transform.position);
+        return new Vector2(pos.x, pos.y);
     }
 }
