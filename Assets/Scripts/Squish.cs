d12bfd03d5eca16be2cebfd029c382d69801af5a
Jakob Ruckel <jakob@ruckel.de>
2022-12-16 18:20:57 +0100

    Squish

---
+++ Assets/Scripts/Squish.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squish : MonoBehaviour {
+    [SerializeField]
+    private List<AudioClip> m_SquishSounds = new List<AudioClip>();
+
+    private float m_Cooldown = 3f;
+
+    private float m_Chance = 0.6f;
+
+    private float m_LastPlayed = 0;
+
+    void Awake() {
+        m_Source = GetComponent<AudioSource>();
+    }
+
+    private AudioSource m_Source;
+
+    void OnCollisionEnter(Collision c) {
+        if (UnityEngine.Random.Range(0f, 1f) < m_Chance) {
+            PlaySquish();
+        }
+    }
+
+    public void PlaySquish() {
+        if (m_Source.isPlaying) {
+            return;
+        }
+
+        var time = Time.fixedTime;
+
+        if (m_LastPlayed + m_Cooldown > time) {
+            return;
+        }
+
+        var clip = Util.RandomChoice<AudioClip>(m_SquishSounds);
+        m_Source.PlayOneShot(clip);
+        m_LastPlayed = time;
+    }
+}
--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Util : MonoBehaviour {
@@ -8,5 +9,13 @@ public class Util : MonoBehaviour {
     public static Vector2 DivideElementWise2(Vector2 a, Vector2 b) {
         var res = DivideElementWise3(new Vector3(a.x, a.y, 1), new Vector3(b.x, b.y, 1));
         return new Vector2(res.x, res.y);
+    }
+
+    public static T RandomChoice<T>(List<T> list) where T: UnityEngine.Object {
+        int idx = UnityEngine.Random.Range(0, list.Count);
+        if (idx < 0 || idx >= list.Count) {
+            return null;
+        }
+        return list[idx];
     }
 }
